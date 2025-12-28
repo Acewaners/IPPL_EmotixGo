282c79f30b2bb9a64bc6b2c929370f4f4dbe4538
Acewaners <hariyatunnasifa.com>
2025-12-28 23:20:31 +0800

    CORS ALLOWED ORIGIN Value changes..

--- emotix-backend/config/cors.php
+++ emotix-backend/config/cors.php
@@ -1,11 +1,29 @@
 <?php
+
 return [
-    'paths' => ['api/*','login','logout','register','sanctum/csrf-cookie'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Vite dev
+
+    // BAGIAN INI YANG DIUBAH MENJADI DINAMIS:
+    // Artinya: "Baca dari Env Variable Railway. Kalau tidak ada, baru pakai localhost"
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173')),
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
+
 ];
